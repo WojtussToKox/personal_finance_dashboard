f9628cb85417e329053238eb57aeea229093368a
Wojciech <wojtek.p8@op.pl>
2026-05-28 13:48:21 +0200

    feat: first django start and 2 models

---
+++ backend/expenses/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class Expense(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    count = models.IntegerField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} -> {self.price}PLN x {self.count} -> {self.date}"
