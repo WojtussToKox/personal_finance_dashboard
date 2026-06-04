3acdb9f9c37beeda9ce5768a95e4420b0edca7da
Wojciech <wojtek.p8@op.pl>
2026-06-04 03:37:58 +0200

    feat: added form for adding new Income (UX)

---
+++ frontend/src/pages/IncomeAddForm.jsx
@@ -0,0 +1,12 @@
+import AddIncomeForm from '../components/AddIncomeForm'
+
+function IncomeAddForm() {
+  return (
+    <div>
+      <h2>💸 Dodaj Nowy Przychód</h2>
+      <AddIncomeForm />
+    </div>
+  )
+}
+
+export default IncomeAddForm
