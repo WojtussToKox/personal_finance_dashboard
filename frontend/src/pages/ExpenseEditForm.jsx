74c0eb2d5994a542780d90e12ff80e1d3508a944
Wojciech <wojtek.p8@op.pl>
2026-06-05 16:09:37 +0200

    feat: added edit forms for incomes and expenses

---
+++ frontend/src/pages/ExpenseEditForm.jsx
@@ -0,0 +1,14 @@
+import EditExpenseForm from "../components/EditExpenseForm";
+
+function ExpenseEditForm () {
+    return (
+        <>
+            <div>
+                <h2>EDYTUJ PODANY WYDATEK</h2>
+            </div>
+            <EditExpenseForm />
+        </>
+    )
+}
+
+export default ExpenseEditForm
